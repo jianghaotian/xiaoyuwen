b069e0b1481c3ea1301bb7f2dc4692e98d7a9a47
wanglinawln <1416978302@qq.com>
2019-11-26 14:16:59 +0800

    add

---
+++ src/app/src/container/js/Chengyu/Chengyu.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Chengyu extends Component {
+    render() {
+        return (
+            <div>
+                Chengyu
+            </div>
+        )
+    }
+}
---
+++ src/app/src/container/js/Pinyin/Pinyin.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { NavBar} from 'antd-mobile';
+import '../../css/Pinyin/pinyin.css';
+export default class Pinyin extends Component {
+    render() {
+        return (
+            <div className="p1">
+                <NavBar
+                style={{backgroundColor:"#6189B8",color:"#fff"}}
+                >拼音</NavBar>
+
+            </div>
+        )
+    }
+}
