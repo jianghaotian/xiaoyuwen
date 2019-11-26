d3f7f9b0b4b5974fc3ba56408450bfc08c9d338b
unknown <815400083@qq.com>
2019-11-26 17:07:59 +0800

    w3

--- src/app/src/container/Chengyu/Idiomscon.js
+++ src/app/src/container/Chengyu/Idiomscon.js
@@ -1,44 +1,23 @@
 import React, { Component } from 'react'
-import {NavBar,SearchBar,TextareaItem,Icon} from 'antd-mobile';
 export default class Idiomscon extends Component {
+    change=(e)=>{
+        e.target.className=(e.target.className=="iconfont icon-xingxing black")?"iconfont icon-xingxing1 yello":"iconfont icon-xingxing black"
+    }
     render() {
         return (
             <div>
-                 <NavBar
-                    icon={<Icon type="left"/>}
-                    onLeftClick={() => console.log('onLeftClick')}
-                    style={{backgroundColor:"#617ca6"}}
-                    >学 成 语</NavBar>
-                
-                <div className="learnidiombody"> 
-                    
-                    <div className="learnidiomcon">
-                        <SearchBar
-                            placeholder="查找"
-                            onSubmit={value => console.log(value, 'onSubmit')}
-                            onClear={value => console.log(value, 'onClear')}
-                            onFocus={() => console.log('onFocus')}
-                            onBlur={() => console.log('onBlur')}
-                            onCancel={() => console.log('onCancel')}
-                            onChange={this.onChange}
-                            style={{height:"2rem"}}
-                        />
-                        <div className="orange">第<span> 1</span><span> / </span><span>100 </span>个</div>
-                        <div className="iconfont icon-xingxing" style={{fontSize:22,marginLeft:"90%"}}></div>
-                        <div className="idiomcon">
-                            <div><img className="learnimg" src={require("../../../images/learnbackground.jpg") }/></div>
-                            <div><span>成语: </span><span>{this.props.content.chengyu}</span><span className="iconfont icon-laba1" style={{fontSize:22,color:"#617ca6",marginLeft:"10%"}}></span></div>
-                            <div><span>释义: </span>{this.props.content.shiyi}</div>
-                            <div><span>比喻: </span>{this.props.content.biyu}</div>
-                            <div><span>例句: </span>{this.props.content.liju}</div>
-                            <img src={this.props.content.image}/>
-                            <textarea value={this.props.content.story}></textarea>
-                        </div>
-                    </div>
-                    
+                <div className="orange">第<span> {this.props.num}</span><span> / </span><span>100 </span>个</div>
+                <div onClick={this.change} className="iconfont icon-xingxing black"></div>
+                <div className="idiomcon">
+                    <div><img className="learnimg" src={require("../../../images/learnbackground.jpg") }/></div>
+                    <div><span>成语: </span><span>{this.props.content.chengyu}</span><span className="iconfont icon-laba1" style={{fontSize:22,color:"#617ca6",marginLeft:"10%"}}></span></div>
+                    <div><span>释义: </span>{this.props.content.shiyi}</div>
+                    <div><span>比喻: </span>{this.props.content.biyu}</div>
+                    <div><span>例句: </span>{this.props.content.liju}</div>
+                    <img src={this.props.content.image}/>
+                    <textarea value={this.props.content.story}></textarea>
                 </div>
-                
-            </div>
+            </div>   
         )
     }
 }
