090a0f89579a74a89aee80a8bd0754c590c69d0b
unknown <815400083@qq.com>
2019-11-26 09:15:30 +0800

    wxt

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import LearnIdioms from './LearnIdioms'
+import {BrowserRouter as Router,Route,Link} from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Route path="/shouye" component={Shouye}/>
+          <Route path="/shengmu" component={Shengmu}/>
+          <Route path="/yunmu" component={Yunmu}/>
+          <Route path="/zhengtiyin" component={Zhengtiyin}/>
+          <Route path="/kanzishiyin" component={Kanzishiyin}/>
+          <Route path="/tingyinxuanzi" component={Tingyinxuanzi}/>
+          <Route path="/xuechengyu" component={LearnIdioms}/>
+          <Route path="/chengyujielong" component={Chengyujielong}/>
+          <Route path="/chengyuyicuozi" component={Chengyuyicuozi}/>
+          <Route path="/caichengyu" component={Caichengyu}/>
+          <Route path="/guanqia" component={}/>
+          <Route path="/xueshici" component={Xueshici}/>
+          <Route path="/buchongshiju" component={Buchongshiju}/>
+          <Route path="/wode" component={Wode}/>
+          <Route path="/jindu" component={Jindu}/>
+          <Route path="/gerenxinxi" component={Gerenxinxi}/>
+          <Route path="/shezhi" component={Shezhi}/>
+          <Route path="/shoucang" component={Shoucang}/>
+        </Router>
+      </div>
+    )
+  }
 }
 
-export default App;
