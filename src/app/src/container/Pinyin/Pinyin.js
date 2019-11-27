517d32eba5a8423b0bbc6c9d2a4aefc47c1cef10
wanglinawln <1416978302@qq.com>
2019-11-27 08:58:55 +0800

    add

--- src/app/src/container/Pinyin/Pinyin.js
+++ src/app/src/container/Pinyin/Pinyin.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { NavBar} from 'antd-mobile';
+import {Link} from 'react-router-dom'
 import '../../css/Pinyin/pinyin.css';
 export default class Pinyin extends Component {
     render() {
@@ -8,7 +9,31 @@ export default class Pinyin extends Component {
                 <NavBar
                 style={{backgroundColor:"#6189B8",color:"#fff"}}
                 >拼音</NavBar>
-                    gdeyuw
+                <div className="p2 p7">
+                    <div className="p3 p5">
+                        <Link to="/"><button className="p4">声母</button></Link>
+                    </div>
+                </div>
+                <div className="p2">
+                    <div className="p3 p6">
+                        <Link to="/"><button className="p4">看字识音</button></Link>
+                    </div>
+                </div>
+                <div className="p2">
+                    <div className="p3 p5">
+                        <Link to="/"><button className="p4">韵母</button></Link>
+                    </div>
+                </div>
+                <div className="p2">
+                    <div className="p3 p6">
+                        <Link to="/"><button className="p4">听音选字</button></Link>
+                    </div>
+                </div>
+                <div className="p2">
+                    <div className="p3 p5">
+                        <Link to="/"><button className="p4">整体音</button></Link>
+                    </div>
+                </div>
             </div>
         )
     }
