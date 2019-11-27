aaae78cf89990c61191558e8c79d992d0270046f
wanglinawln <1416978302@qq.com>
2019-11-27 09:05:28 +0800

    add

--- src/app/src/container/Chengyu/Chengyu.js
+++ src/app/src/container/Chengyu/Chengyu.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
+import { NavBar} from 'antd-mobile';
+import {Link} from 'react-router-dom'
+import '../../css/Pinyin/pinyin.css';
+import '../../css/Chengyu/chengyu.css';
 
 export default class Chengyu extends Component {
     render() {
         return (
-            <div>
-                Chengyu
+            <div className="p1">
+                <NavBar
+                style={{backgroundColor:"#6189B8",color:"#fff"}}
+                >成语</NavBar>
+                <div className="p2 c1">
+                    <div className="p3 p5">
+                        <Link to="/"><button className="p4">学成语</button></Link>
+                    </div>
+                </div>
+                <div className="p2">
+                    <div className="p3 p6">
+                        <Link to="/"><button className="p4">成语接龙</button></Link>
+                    </div>
+                </div>
+                <div className="p2">
+                    <div className="p3 p5">
+                        <Link to="/"><button className="p4">易错字</button></Link>
+                    </div>
+                </div>
+                <div className="p2">
+                    <div className="p3 p6">
+                        <Link to="/"><button className="p4">猜成语</button></Link>
+                    </div>
+                </div>
             </div>
         )
     }
