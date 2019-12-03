173d45d23eeefa8eaba9a223b2d85fe26f38e0b7
luyishuang <1781990652@qq.com>
2019-12-03 11:00:17 +0800

    lu

--- src/app/src/components/WoDe/Shoucangjia.js
+++ src/app/src/components/WoDe/Shoucangjia.js
@@ -17,21 +17,21 @@ export default class Shoucangjia extends Component {
             <div>
                 <div className="wode_back"></div>
                 <NavBar mode="dark" icon={<Icon type="left" onClick={()=>{this.props.history.push('/home/wode')}}/>} style={{background:'#617ca6',color:'#fff'}}>收 藏 夹</NavBar>
-                <div className="scj_tab">
+                <div className="scj_tab" onClick={()=>{this.props.history.push('/wode/shoucang/pinyin')}}>
                     <i className={'iconfont icon-pinyin scc_i'}></i>
                     <span className="scj_tab_text">拼音</span>
                     <span className="scj_you" style={{fontSize:'13px'}}>
                         <i style={{marginLeft:'1rem'}} className={'iconfont icon-youjiantou'}></i>
                     </span>
                 </div>
-                <div className="scj_tab">
+                <div className="scj_tab" onClick={()=>{this.props.history.push('/wode/shoucang/chengyu')}}>
                     <i className={'iconfont icon-cy scc_i'}></i>
                     <span className="scj_tab_text">成语</span>
                     <span className="scj_you" style={{fontSize:'13px'}}>
                         <i style={{marginLeft:'1rem'}} className={'iconfont icon-youjiantou'}></i>
                     </span>
                 </div>
-                <div className="scj_tab">
+                <div className="scj_tab" onClick={()=>{this.props.history.push('/wode/shoucang/shici')}}>
                     <i className={'iconfont icon-shicidaquan scc_i'}></i>
                     <span className="scj_tab_text">诗词</span>
                     <span className="scj_you" style={{fontSize:'13px'}}>
