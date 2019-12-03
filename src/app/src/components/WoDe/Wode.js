b194f53a5c1027754455cfd10ba5cf5b6d4cda8f
lemonoink <jianghaotian126@126.com>
2019-12-03 14:11:24 +0800

    jht add login

--- src/app/src/components/WoDe/Wode.js
+++ src/app/src/components/WoDe/Wode.js
@@ -7,29 +7,33 @@ export default class Wode extends Component {
     constructor() {
         super();
         this.state = {
-
+            login: false,
+            head: require('../../images/headImage.jpg'),
+            name: '未登录',
+            signature: '点击此处登录',
+            grade: '一年级'
         };
     }
 
     // TODO
-    componentDidMount() {
-        api.getinfo().then(res => {
-            console.log(res);
+    // componentDidMount() {
+    //     api.getinfo().then(res => {
+    //         console.log(res);
 
-        });
+    //     });
 
+    // }
 
-    }
     render() {
         return (
             <div>
                 <NavBar mode="dark" style={{background:'#617ca6',color:'#fff'}}>我 的</NavBar>
-                <div className="wode_info a_click" onClick={()=>{this.props.history.push('/wode/info')}}>
+                <div className="wode_info a_click" onClick={()=>{this.state.login ? this.props.history.push('/wode/info') : this.props.history.push('/login')}}>
                     <div style={{height:'90%',padding:'1rem'}}>
-                        <img className="wode_header" src={require('../../images/headImage.jpg')} />
+                        <img className="wode_header" src={this.state.head} />
                         <div style={{float:"right",width:'70%'}}>
-                            <div style={{float:"left",fontSize:'20px',minWidth:'100%'}}>姜皓天</div>
-                            <div style={{float:"left",fontSize:'15px',marginTop:'1rem'}}>这里是个性签名，限制25个字</div>
+                            <div style={{float:"left",fontSize:'20px',minWidth:'100%'}}>{this.state.name}</div>
+                            <div style={{float:"left",fontSize:'15px',marginTop:'1rem'}}>{this.state.signature}</div>
                         </div>
                     </div>
                 </div>
@@ -37,7 +41,7 @@ export default class Wode extends Component {
                     <i className={'iconfont icon-nianji wode_i'}></i>
                     <span className="wode_tab_text">年级</span>
                     <span className="wode_you" style={{fontSize:'13px'}}>
-                        一年级
+                        {this.state.grade}
                         <i style={{marginLeft:'1rem'}} className={'iconfont icon-youjiantou'}></i>
                     </span>
                 </div>
