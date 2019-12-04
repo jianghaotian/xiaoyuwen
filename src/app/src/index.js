ce2646ef93bc20bfc7acae7129d9ea624c253e7c
lemonoink <jianghaotian126@126.com>
2019-12-04 15:17:21 +0800

    jht add

--- src/app/src/index.js
+++ src/app/src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'antd-mobile/dist/antd-mobile.css';
 import App from './App';
+
 import api from './request/api';
+import store from './redux/store';
 // import * as serviceWorker from './serviceWorker';
 
 React.Component.prototype.$api = api;
+React.Component.prototype.$store = store;
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
