ec2c9e2e9f567d93ceaf7eb7b76eb8573667848d
lemonoink <jianghaotian126@126.com>
2019-12-04 14:29:50 +0800

    jht add

--- src/app/src/redux/actions/index.js
+++ src/app/src/redux/actions/index.js
@@ -1,29 +1,29 @@
 import makeActionCreator from './makeActionCreator';
 
-
-
-
-
 // token
 export const SET_TOKEN = 'SET_TOKEN';
-
 export const setToken = makeActionCreator(SET_TOKEN, 'token')
 
+export const SET_UID = 'SET_UID';
+export const setUid = makeActionCreator(SET_UID, 'uid');
+
+export const SET_TOKEN_ALL = 'SET_TOKEN_ALL';
+export const setTokenAll = makeActionCreator(SET_TOKEN_ALL, 'token', 'uid');
+
+export const CLEAR_TOKEN_ALL = 'CLEAR_TOKEN_ALL'
+export const clearTokenAll = makeActionCreator(CLEAR_TOKEN_ALL);
 
 
 // users
 
-export const SET_UID = 'SET_UID';
 export const SET_USERS = 'SET_USERS';
-
-
-export const setUid = makeActionCreator(SET_UID, 'uid');
 export const setUsers = makeActionCreator(SET_USERS, 'users');
 
+export const CLEAR_USERS = 'CLEAR_USERS';
+export const clearUsers = makeActionCreator(CLEAR_USERS);
 
-
-
-
+export const SET_GRADE = 'SET_GRADE';
+export const setGrade = makeActionCreator(SET_GRADE, 'grade');
 
 
 
