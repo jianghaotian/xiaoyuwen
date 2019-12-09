4c298cda7b71a49595dbdfb7524c98569ae3f344
lemonoink <jianghaotian126@126.com>
2019-12-09 13:57:56 +0800

    jht add

---
+++ src/admin/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="app">
+        <router-view/>
+    </div>
+</template>
+
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+html { 
+    height:100%; 
+}
+
+body { 
+    height:100%;
+}
+#app {
+    width: 100%;
+    height: 100%;
+    font-family: "Helvetica Neue", Helvetica, "PingFang SC", "Hiragino Sans GB", "Microsoft YaHei", "微软雅黑", Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+}
+
+</style>
