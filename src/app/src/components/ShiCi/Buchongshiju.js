e8a44d7fd310f65449fa96ba1f19df1eafa908c9
luyishuang <1781990652@qq.com>
2019-12-11 14:45:22 +0800

    lys

--- src/app/src/components/ShiCi/Buchongshiju.js
+++ src/app/src/components/ShiCi/Buchongshiju.js
@@ -1,94 +1,187 @@
 import React, { Component } from 'react';
-import { NavBar, Icon,Toast } from 'antd-mobile';
+import { NavBar, Icon,Toast,Button } from 'antd-mobile';
 import '../../css/ShiCi/Buchongshiju.css';
 
 export default class Buchongshiju extends Component {
     constructor(){
         super();
-        this.content={
-            choose:["低头思故乡。","疑是地上霜。","举头望明月。","粒粒皆辛苦。"],
-            answer:"疑是地上霜。"
-        }
         this.state={
             num:1,
-            next:"下一题"
-        }
-    }
-    getAnswer=(e)=>{
-        var answer=this.refs.answer
-        answer.innerHTML=e.target.innerHTML;
-    }
-    adds=(e)=>{
-        var answer=this.refs.answer
-        if(answer.innerHTML==this.content.answer){
-            Toast.info("恭喜你，答对啦！撒花🎉！！",1,()=>{
-                this.setState((state)=>{
-                    if(state.num==3){
-                        return{
-                            num:3
-                        }
-                    }
-                    return{
-                        num:++state.num
-                    }
-                })
-                if(this.state.num==3){
-                    this.setState({
-                        next:"下一关"
-                    })
+            scsubmitFront:'scsubmit-front',
+            answerArr:{},
+            front:'上一题',
+            next:"下一题",
+            shang:'',
+            xia:'',
+            shangStyle:{borderBottom:'#fff 1px solid'},
+            xiaStyle:{borderBottom:'#fff 1px solid'},
+            /**0为填下句 */
+            data:{
+                1:{
+                    flag:0,
+                    show:'床前明月光哈哈。',
+                    choose:["低头思故乡哈哈。","疑是地上霜。","举头望明月。","粒粒皆辛苦。"],
+                },
+                2:{
+                    flag:1,
+                    show:'疑是地上霜。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","床前明月光。"],
+                },
+                3:{
+                    flag:0,
+                    show:'床前明月光。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","粒粒皆辛苦。"],
+                },
+                4:{
+                    flag:1,
+                    show:'疑是地上霜。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","床前明月光。"],
+                },
+                5:{
+                    flag:0,
+                    show:'床前明月光。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","粒粒皆辛苦。"],
+                },
+                6:{
+                    flag:1,
+                    show:'疑是地上霜。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","床前明月光。"],
+                },
+                7:{
+                    flag:0,
+                    show:'床前明月光。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","粒粒皆辛苦。"],
+                },
+                8:{
+                    flag:1,
+                    show:'疑是地上霜。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","床前明月光。"],
+                },
+                9:{
+                    flag:0,
+                    show:'床前明月光。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","粒粒皆辛苦。"],
+                },
+                10:{
+                    flag:1,
+                    show:'疑是地上霜。',
+                    choose:["低头思故乡。","疑是地上霜。","举头望明月。","床前明月光。"],
                 }
+            }
+    }
+}
+    getAnswer=(item)=>{
+        if(this.state.data[this.state.num].flag==0){
+            this.setState({
+                xia:item
             })
         }
         else{
-            Toast.info("太遗憾了，你答错了！😭",1,()=>{
-                this.setState((state)=>{
-                    if(state.num==3){
-                        return{
-                            num:3
-                        }
-                    }
-                    return{
-                        num:++state.num
-                    }
-                })
-                if(this.state.num==3){
-                    this.setState({
-                        next:"下一关"
-                    })
-                }
-            }) 
+            this.setState({
+                shang:item
+            })
         }
+        let answer = {...this.state.answerArr};
+        answer[this.state.num] = item;
+        this.setState({
+            answerArr:answer
+        })
+    }
+    componentDidMount(){
+        this.createQuestion();
+    }
+    createQuestion=()=>{
+        if(this.state.data[this.state.num].flag==0){
+            this.setState({
+                shang:this.state.data[this.state.num].show,
+                xia:this.state.answerArr[this.state.num] || '',
+                shangStyle:{borderBottom:'#fff 1px solid'},
+                xiaStyle:{borderBottom:'rgb(0, 0, 0) 1px solid'}
+            })
+        }
+        else{
+            this.setState({
+                shang:this.state.answerArr[this.state.num] || '',
+                xia:this.state.data[this.state.num].show,
+                shangStyle:{borderBottom:'rgb(0, 0, 0) 1px solid'},
+                xiaStyle:{borderBottom:'#fff 1px solid'}
+            })
+        }
+    }
+    adds=(e)=>{
+        this.setState({
+            scsubmitFront:'scsubmit-front1'
+        })
+        if(this.state.num == 9){
+            this.setState({
+                next:'提 交'
+            })
+        }
+        else if(this.state.num < 10){
+            this.setState({
+                next:'下一题'
+            })
+        }
+        this.setState((state)=>{
+            if(state.num==10){
+                return{
+                    num:10
+                }
+            }
+            return{
+                num:++state.num
+            }
+        },()=>this.createQuestion())
+    }
+    less=(e)=>{
+        if(this.state.num <= 10){
+            this.setState({
+                next:'下一题'
+            })
+            // this.createQuestion();
+        }
+        if(this.state.num == 2){
+            this.setState({
+                scsubmitFront:'scsubmit-front'
+            })
+        }
+        this.setState((state)=>{
+            if(state.num==1){
+                return{
+                    num:1
+                }
+            }
+            return{
+                num:--state.num
+            }
+        },()=>this.createQuestion())
     }
     render() {
         return (
             <div>
                 <NavBar
                     mode="dark"
-                    icon={<Icon type="left" />}
+                    icon={<Icon type="left" onClick={()=>{this.props.history.push('/home/shici')}}/>}
                     style={{background:'#617ca6',color:'#fff',height:'55px'}}
                     >补 充 诗 句
                 </NavBar>
                 <div className="bcsj_back"></div>
                 <div className="bcsj_box">
-                <p><span>第 </span><span className="bcsj_text1"><span>{this.state.num}</span>/3</span><span> 个</span></p>
+                <p><span>第 </span><span className="bcsj_text1"><span>{this.state.num}</span>/10</span><span> 个</span></p>
                     <div className="bcsj_poetry">
                         <div className="iconfont icon-tuding tuding"></div>
-                        <div style={{margin:'0 auto',textShadow:"1px 1px gray,-1px -1px rgb(241, 241, 241)"}}>床前明月光，</div>
-                        <div ref="answer" id="answer" className="bcsj_poetry_text">
-
-                            
-                        </div>
+                        <div id="answer" className="bcsj_poetry_text" style={this.state.shangStyle}>{this.state.shang}</div>
+                        <div id="answer" className="bcsj_poetry_text" style={this.state.xiaStyle}>{this.state.xia}</div>
                         <div style={{marginTop:'20%'}}>
                             {
-                                (this.content.choose).map((item,index)=>{
-                                    return <div onClick={this.getAnswer} className="bcsj_option">{item}</div>
+                                (this.state.data[this.state.num].choose).map((item,index)=>{
+                                    return <div key={index} onClick={()=>{this.getAnswer(item)}} className="bcsj_option">{item}</div>
                                 })
                             }
-                            
-                            
                         </div>
                         <div className="scoutsubmit">
-                            <button onClick={this.adds} class="scsubmit">{this.state.next}</button>
+                            <Button onClick={this.less} className={this.state.scsubmitFront}>{this.state.front}</Button>
+                            <Button onClick={this.adds} className="scsubmit-next">{this.state.next}</Button>
                         </div>
                     </div>
                     
