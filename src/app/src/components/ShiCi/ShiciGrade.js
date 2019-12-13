52ed718342c6830fc1daf5ee78542da4eb7161f8
luyishuang <1781990652@qq.com>
2019-12-13 16:05:35 +0800

    lys

--- src/app/src/components/ShiCi/ShiciGrade.js
+++ src/app/src/components/ShiCi/ShiciGrade.js
@@ -9,55 +9,55 @@ export default class ShiciGrade extends Component {
             <div>
                 <div className="grade-back"></div>
                 <Grade />
-                <div style={{minHeight:'40rem'}}>
+                <div style={{minHeight:'53rem',marginTop:'5%'}}>
                     <div className='sc-listBox'>
                         <span className="sc-text">1.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-zhengque2 sc-panduan'}></i>
+                        <i className={'iconfont icon-zhengque2 sc-panduandui'}></i>
                     </div>
                     <div className='sc-backtrue'>答对啦！</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">2.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-cuowu2 sc-panduan'}></i>
+                        <i className={'iconfont icon-cuowu2 sc-panduancuo'}></i>
                     </div>
                     <div className='sc-backerr'>答案是:床前明月光，疑是地上霜</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">3.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-zhengque2 sc-panduan'}></i>
+                        <i className={'iconfont icon-zhengque2 sc-panduandui'}></i>
                     </div>
                     <div className='sc-backtrue'>答对啦！</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">4.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-cuowu2 sc-panduan'}></i>
+                        <i className={'iconfont icon-cuowu2 sc-panduancuo'}></i>
                     </div>
                     <div className='sc-backerr'>答案是:床前明月光，疑是地上霜</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">5.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-zhengque2 sc-panduan'}></i>
+                        <i className={'iconfont icon-zhengque2 sc-panduandui'}></i>
                     </div>
                     <div className='sc-backtrue'>答对啦！</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">6.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-cuowu2 sc-panduan'}></i>
+                        <i className={'iconfont icon-cuowu2 sc-panduancuo'}></i>
                     </div>
                     <div className='sc-backerr'>答案是:床前明月光，疑是地上霜。少时诵诗书。双双双双是。</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">7.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-zhengque2 sc-panduan'}></i>
+                        <i className={'iconfont icon-zhengque2 sc-panduandui'}></i>
                     </div>
                     <div className='sc-backtrue'>答对啦！</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">8.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-zhengque2 sc-panduan'}></i>
+                        <i className={'iconfont icon-zhengque2 sc-panduandui'}></i>
                     </div>
                     <div className='sc-backtrue'>答对啦！</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">9.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-zhengque2 sc-panduan'}></i>
+                        <i className={'iconfont icon-zhengque2 sc-panduandui'}></i>
                     </div>
                     <div className='sc-backtrue'>答对啦！</div>
                     <div className='sc-listBox'>
                         <span className="sc-text">10.床前明月光，疑是地上霜</span>
-                        <i className={'iconfont icon-zhengque2 sc-panduan'}></i>
+                        <i className={'iconfont icon-zhengque2 sc-panduandui'}></i>
                     </div>
                     <div className='sc-backtrue'>答对啦！</div>
                 </div>
