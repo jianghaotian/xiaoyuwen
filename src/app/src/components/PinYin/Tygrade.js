19bdf585d8152bb0da3fe90eed4c78df37fa2463
wanglinawln <1416978302@qq.com>
2019-12-16 17:12:37 +0800

    add

--- src/app/src/components/PinYin/Tygrade.js
+++ src/app/src/components/PinYin/Tygrade.js
@@ -3,70 +3,74 @@ import '../../css/PinYin/tygrade.css'
 import {NavBar,Icon} from 'antd-mobile';
 
 export default class Tygrade extends Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state={
-            score:3,
-            data:[
-                {
-                    write:'早',
-                    answer:"早",
-                    status:true    
-                },
-                {
-                    write:'早',
-                    answer:"上",
-                    status:false   
-                },
-                {
-                    write:'早',
-                    answer:"我",
-                    status:false    
-                },
-                {
-                    write:'早',
-                    answer:"吃",
-                    status:false   
-                },
-                {
-                    write:'早',
-                    answer:"了",
-                    status:false   
-                },
-                {
-                    write:'早',
-                    answer:"面",
-                    status:false    
-                },
-                {
-                    write:'早',
-                    answer:"包",
-                    status:false   
-                },
-                {
-                    write:'面',
-                    answer:"和",
-                    status:false   
-                },
-                {
-                    write:'鸡',
-                    answer:"鸡",
-                    status:true    
-                },
-                {
-                    write:'蛋',
-                    answer:"蛋",
-                    status:true    
-                }
+            score:this.props.location.state.score,
+            // data:[
+            //     {
+            //         write:'早',
+            //         answer:"早",
+            //         status:true    
+            //     },
+            //     {
+            //         write:'早',
+            //         answer:"上",
+            //         status:false   
+            //     },
+            //     {
+            //         write:'早',
+            //         answer:"我",
+            //         status:false    
+            //     },
+            //     {
+            //         write:'早',
+            //         answer:"吃",
+            //         status:false   
+            //     },
+            //     {
+            //         write:'早',
+            //         answer:"了",
+            //         status:false   
+            //     },
+            //     {
+            //         write:'早',
+            //         answer:"面",
+            //         status:false    
+            //     },
+            //     {
+            //         write:'早',
+            //         answer:"包",
+            //         status:false   
+            //     },
+            //     {
+            //         write:'面',
+            //         answer:"和",
+            //         status:false   
+            //     },
+            //     {
+            //         write:'鸡',
+            //         answer:"鸡",
+            //         status:true    
+            //     },
+            //     {
+            //         write:'蛋',
+            //         answer:"蛋",
+            //         status:true    
+            //     }
 
-            ]
+            // ],
+            data:this.props.location.state.data
         }
     }
     render() {
         return (
             <div>
                 <div className="ty-grade-back"></div>
-                <NavBar icon={<Icon type="left" onClick={()=>{this.props.history.push('/home/pinyin')}} />} style={{backgroundColor:"#617ca6",position:"fixed",width:"100%",zIndex:"10"}}>听 音 选 字</NavBar>
+                <NavBar
+                    icon={<Icon type="left" onClick={()=>{this.props.history.push('/home/pinyin')}} />}
+                    style={{backgroundColor:"#617ca6",position:"fixed",width:"100%",zIndex:"10"}}
+                    >听 音 选 字</NavBar>
                 <div className="ty1">
                     
                     <img className="ty2" src={require('../../images/word3.PNG')} />
