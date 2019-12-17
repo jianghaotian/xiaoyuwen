4ae409a8ab940fb0b741ecbe88b070d32f07ddc4
lemonoink <jianghaotian126@126.com>
2019-12-17 08:27:13 +0800

    jht a

--- src/server/routes/data.js
+++ src/server/routes/data.js
@@ -39,6 +39,161 @@ router.get('/xpy', function (req, res, next) {
     res.json(jsonData);
 });
 
+/**
+ * （看字拾音判题后存储）
+ * POST
+ * 接收参数:
+ *     answer : 用户答案信息
+ * 返回参数:
+ *     status: 0,
+ *     message: "OK",
+ *     data: {}
+ */
+router.post('/kzsy', function (req, res, next) {
+    let { answer } = req.body;
+    let token = req.header('token');
+
+    checkToken(token, (result) => {
+        let num = 0;
+        answer.forEach(value => {
+            if (value.flag) {
+                num++;
+            }
+        });
+        if (result.status === 0) {
+            let day = getTimestamp_13();
+            runSql('insert into question(Uid, Qtype, Qscore, Qday) values (?,?,?,?)', [result.data.uid, 'kzsy', num, day], (result1) => {
+                if (result1.status === 0) {
+                    let fileCont = {
+                        answer: answer,
+                        grade: num
+                    }
+                    let filePath = path.join(__dirname, '../data/users/kzsy/' + day + '.json');
+                    let str = JSON.stringify(fileCont);
+                    fs.writeFile(filePath, str, (err) => {
+                        if (err) {
+                            let jsonData = {
+                                status: -1,
+                                message: 'write file error'
+                            }
+                            res.json(jsonData);
+                        } else {
+                            let jsonData = {
+                                status: 0,
+                                data: day
+                            }
+                            res.json(jsonData);
+                        }
+                    });
+                } else {
+                    res.json(result1);
+                }
+            });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+/**
+ * （看字拾音成绩单）
+ * GET
+ * 接收参数:
+ *     time : 文件名
+ * 返回参数:
+ *     status: 0,
+ *     message: "OK",
+ *     data: {}
+ */
+router.get('/kzsy/grade', function (req, res, next) {
+    let { time } = req.query;
+    // let token = req.header('token');
+    let data = require(path.join(__dirname, '../data/users/kzsy/' + time + '.json'));
+
+    let jsonData = {
+        status: 0,
+        data: data
+    }
+    res.json(jsonData);
+});
+
+/**
+ * （听音选字判题后存储）
+ * POST
+ * 接收参数:
+ *     answer : 用户答案信息
+ * 返回参数:
+ *     status: 0,
+ *     message: "OK",
+ *     data: {}
+ */
+router.post('/tyxz', function (req, res, next) {
+    let { answer } = req.body;
+    let token = req.header('token');
+
+    checkToken(token, (result) => {
+        let num = 0;
+        answer.forEach(value => {
+            if (value.flag) {
+                num++;
+            }
+        });
+        if (result.status === 0) {
+            let day = getTimestamp_13();
+            runSql('insert into question(Uid, Qtype, Qscore, Qday) values (?,?,?,?)', [result.data.uid, 'tyxz', num, day], (result1) => {
+                if (result1.status === 0) {
+                    let fileCont = {
+                        answer: answer,
+                        grade: num
+                    }
+                    let filePath = path.join(__dirname, '../data/users/tyxz/' + day + '.json');
+                    let str = JSON.stringify(fileCont);
+                    fs.writeFile(filePath, str, (err) => {
+                        if (err) {
+                            let jsonData = {
+                                status: -1,
+                                message: 'write file error'
+                            }
+                            res.json(jsonData);
+                        } else {
+                            let jsonData = {
+                                status: 0,
+                                data: day
+                            }
+                            res.json(jsonData);
+                        }
+                    });
+                } else {
+                    res.json(result1);
+                }
+            });
+        } else {
+            res.json(result);
+        }
+    });
+});
+
+/**
+ * （看字拾音成绩单）
+ * GET
+ * 接收参数:
+ *     time : 文件名
+ * 返回参数:
+ *     status: 0,
+ *     message: "OK",
+ *     data: {}
+ */
+router.get('/tyxz/grade', function (req, res, next) {
+    let { time } = req.query;
+    // let token = req.header('token');
+    let data = require(path.join(__dirname, '../data/users/tyxz/' + time + '.json'));
+
+    let jsonData = {
+        status: 0,
+        data: data
+    }
+    res.json(jsonData);
+});
 
 
 // ---------- 成 语 ---------- //
@@ -172,7 +327,7 @@ router.post('/bcsj', function (req, res, next) {
 
 /**
  * （补充诗句成绩单）
- * POST
+ * GET
  * 接收参数:
  *     time : 文件名
  * 返回参数:
