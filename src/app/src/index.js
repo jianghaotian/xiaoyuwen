6ac82a1881246029ebce940cba17c8d663e05772
lemonoink <jianghaotian126@126.com>
2019-12-20 19:46:59 +0800

    jht Optimize the code 2

--- src/app/src/index.js
+++ src/app/src/index.js
@@ -6,15 +6,8 @@ import App from './App';
 
 import api from './request/api';
 import store from './redux/store';
-// import * as serviceWorker from './serviceWorker';
 
 React.Component.prototype.$api = api;
 React.Component.prototype.$store = store;
 
-
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
