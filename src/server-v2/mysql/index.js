14b3a3875340da4206ac3b5f55a22fbb1f8c53f0
lemonoink <jianghaotian126@126.com>
2020-04-08 21:57:06 +0800

    jht add server-v2

---
+++ src/server-v2/mysql/index.js
@@ -0,0 +1,44 @@
+var mysql = require('mysql');
+var mysqlconfig = require('../config').mysql;  // 引入mysql连接配置
+var poolextend = require('./poolextend');  // 引入连接池配置
+
+var pool = mysql.createPool(poolextend({}, mysqlconfig));  // 使用连接池，提升性能
+
+function runSql(sql, data, callback) {
+    pool.getConnection(function (err, connection) {
+        if (err) {
+            let jsonData = {
+                status: err.errno,
+                message: err.sqlMessage,
+                data: {
+                    code: err.code
+                }
+            }
+            callback(jsonData);
+        } else {
+            connection.query(sql, data, function(err, result) {
+                if (err) {
+                    let jsonData = {
+                        status: err.errno,
+                        message: err.sqlMessage,
+                        data: {
+                            sql: err.sql,
+                            code: err.code
+                        }
+                    }
+                    callback(jsonData);
+                } else {
+                    let jsonData = {
+                        status: 0,
+                        message: 'OK',
+                        data: result
+                    }
+                    callback(jsonData);
+                }
+                connection.release();
+            });
+        }
+    });
+}
+
+module.exports = runSql;
