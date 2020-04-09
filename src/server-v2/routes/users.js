d4dc8e786221eb72e2890888f12d41bf4aa2e192
lemonoink <jianghaotian126@126.com>
2020-04-09 10:40:51 +0800

    jht add zimu list

--- src/server-v2/routes/users.js
+++ src/server-v2/routes/users.js
@@ -1,16 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 var {appId, appSecret} = require('../config').wechat;
 
 // 登录
-// router.get('/login', function(req, res, next) {
-//   let { code } = req.query;
-//   let url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=authorization_code`
+router.get('/login', function(req, res, next) {
+  let { code } = req.query;
+  let url = `https://api.weixin.qq.com/sns/jscode2session?appid=${appId}&secret=${appSecret}&js_code=${code}&grant_type=authorization_code`
 
+  request(url, function (error, response, body) {
+    if (error) {
+      console.log(error);
 
+    } else {
+      if (response.statusCode == 200) {
+        console.log(body);
+        // "openid":"oGZ330Ce3Kh5dgedN8yhKidV147Q"
 
-//   res.json({login: 'haha'});
+      } else {
+        console.log(response.statusCode);
 
-// });
+      }
+    }
+
+  });
+
+  res.json({login: 'haha'});
+
+});
 
 module.exports = router;
