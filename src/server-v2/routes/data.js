87f5d1d7e17feace5c70225f135ec6a956aac68c
lemonoink <jianghaotian126@126.com>
2020-04-09 13:15:55 +0800

    jht add pinyin api

--- src/server-v2/routes/data.js
+++ src/server-v2/routes/data.js
@@ -1,3 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var { getPinyin } = require('../data/utils/pinyin');
+
+
+router.get('/pinyin', function(req, res, next) {
+  let { id } = req.query;
+  let data = null;
+  if (id) {
+    data = getPinyin(id);
+  }
+  let jsonData = {
+    status: data == null ? -1 : 0,
+    data: data
+  }
+  res.json(jsonData);
+});
 
 
 
+
+module.exports = router;
