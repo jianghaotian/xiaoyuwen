c03c414486f0a08cacc49a299e823e69932ba289
lemonoink <jianghaotian126@126.com>
2020-04-10 11:00:03 +0800

    jht add laba & add tyxzti` laba

--- src/miniprogram/template/laba.js
+++ src/miniprogram/template/laba.js
@@ -1,11 +1,11 @@
-// pages/zimu/zimu.js
+// template/laba.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
   },
 
   /**
@@ -62,18 +62,5 @@ Page({
    */
   onShareAppMessage: function () {
 
-  },
-
-  // 点击播放音频
-  playAudio: function (e) {
-    let {id} = e.currentTarget.dataset;
-    console.log(id);
-
-    wx.createInnerAudioContext(
-
-    );
-
   }
-
-
 })
