d28a6016324a1741cff166288614b133f8ee6fc6
lemonoink <jianghaotian126@126.com>
2020-04-13 16:50:40 +0800

    jht add audio

--- src/server-v2/app.js
+++ src/server-v2/app.js
@@ -13,7 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/v2', express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 app.use('/v2/users', usersRouter);
