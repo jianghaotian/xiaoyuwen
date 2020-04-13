e883fe4aaa1cef909d74c9aabe19678b3c4fefba
lemonoink <jianghaotian126@126.com>
2020-04-13 16:50:22 +0800

    jht change tyxzti

--- src/miniprogram/pages/tyxz/tyxzti.js
+++ src/miniprogram/pages/tyxz/tyxzti.js
@@ -5,12 +5,12 @@ Page({
    * 页面的初始数据
    */
   data: {
-    num:1,
-    answer:'我',
-    duigou:""
-  },
-  select: function (e) {
-    let {id} = e.currentTarget.dataset;
+    num: 1,
+    question: ['我', '是', '又', '又'],
+    answer: '我',
+    duigou: "",
+    labaPlay: 0,
+
   },
 
   /**
@@ -67,5 +67,13 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  select: function (e) {
+    let {id} = e.currentTarget.dataset;
+    console.log(id);
+
+
+  },
+
 })
