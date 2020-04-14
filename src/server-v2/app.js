c817d780eed123f6902175c06ff8cd707ea1553d
lemonoink <jianghaotian126@126.com>
2020-04-14 16:25:03 +0800

    jht add tishi audio

--- src/server-v2/app.js
+++ src/server-v2/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 // var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
+var tyxzRouter = require('./routes/tyxz');
 
 var app = express();
 
@@ -18,5 +19,6 @@ app.use('/v2', express.static(path.join(__dirname, 'public')));
 // app.use('/', indexRouter);
 app.use('/v2/users', usersRouter);
 app.use('/v2/data', dataRouter);
+app.use('/v2/data', tyxzRouter);
 
 module.exports = app;
