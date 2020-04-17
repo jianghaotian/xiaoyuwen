2dc6939f7662315b0e134f709f9dad077c25fd87
lemonoink <jianghaotian126@126.com>
2020-04-17 08:32:09 +0800

    refactor(server-v2): 重构连接数据库的函数

--- src/server-v2/mysql/index.js
+++ src/server-v2/mysql/index.js
@@ -4,41 +4,43 @@ var poolextend = require('./poolextend');  // 引入连接池配置
 
 var pool = mysql.createPool(poolextend({}, mysqlconfig));  // 使用连接池，提升性能
 
-function runSql(sql, data, callback) {
+function runSql(sql, data) {
+  return new Promise((resolve, reject) => {
     pool.getConnection(function (err, connection) {
-        if (err) {
-            let jsonData = {
-                status: err.errno,
-                message: err.sqlMessage,
-                data: {
-                    code: err.code
-                }
-            }
-            callback(jsonData);
-        } else {
-            connection.query(sql, data, function(err, result) {
-                if (err) {
-                    let jsonData = {
-                        status: err.errno,
-                        message: err.sqlMessage,
-                        data: {
-                            sql: err.sql,
-                            code: err.code
-                        }
-                    }
-                    callback(jsonData);
-                } else {
-                    let jsonData = {
-                        status: 0,
-                        message: 'OK',
-                        data: result
-                    }
-                    callback(jsonData);
-                }
-                connection.release();
-            });
+      if (err) {
+        let jsonData = {
+          status: err.errno,
+          message: err.sqlMessage,
+          data: {
+            code: err.code
+          }
         }
+        reject(jsonData);
+      } else {
+        connection.query(sql, data, function (err, result) {
+          if (err) {
+            let jsonData = {
+              status: err.errno,
+              message: err.sqlMessage,
+              data: {
+                sql: err.sql,
+                code: err.code
+              }
+            }
+            reject(jsonData);
+          } else {
+            let jsonData = {
+              status: 0,
+              message: 'OK',
+              data: result
+            }
+            resolve(jsonData);
+          }
+          connection.release();
+        });
+      }
     });
+  });
 }
 
 module.exports = runSql;
