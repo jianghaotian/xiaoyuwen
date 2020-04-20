c55bbd0dfbd21aa57d37377a0a2eed10f631f9b5
lemonoink <jianghaotian126@126.com>
2020-04-20 15:07:35 +0800

    jht add

--- src/server-v2/routes/tyxz.js
+++ src/server-v2/routes/tyxz.js
@@ -11,7 +11,7 @@ router.get('/getguan', async function(req, res, next) {
     let {openid, session_key} = await checkToken(token);
     let sqlResult = await runSql('SELECT guan, score FROM tyxz where uid = ? ORDER BY guan asc', [openid]);
     
-    res.json({status: 0, data: sqlResult, all: 30});
+    res.json({status: 0, data: sqlResult, all: 20});
   } catch(e) {
     console.log(e);
     res.json({status: -1});
