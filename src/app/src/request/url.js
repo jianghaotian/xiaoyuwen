f34d22a6011fa611b07f7b8f19e40e577945840e
lemonoink <jianghaotian126@126.com>
2020-04-29 14:47:55 +0800

    jht change app and server-v2 api

--- src/app/src/request/url.js
+++ src/app/src/request/url.js
@@ -1,20 +1,20 @@
 
-// export let baseUrl = 'http://localhost:8001/v1';  // 后端接口地址
+// export let baseUrl = 'http://localhost:4001/v1';  // 后端接口地址
 export let baseUrl = 'https://xyw.htapi.pub/v1';  // 后端接口地址
 
-export let headUrl = 'https://xyw.htapi.pub/users/headimages/';  // 头像获取地址
+export let headUrl = 'https://xyw.htapi.pub/v1/users/headimages/';  // 头像获取地址
 
-export let pyImgUrl = 'https://xyw.htapi.pub/sys/pyimages/';  // 拼音图片获取地址
-// export let pyImgUrl = 'http://localhost:8001/sys/pyimages/';  // 拼音图片获取地址
+export let pyImgUrl = 'https://xyw.htapi.pub/v1/sys/pyimages/';  // 拼音图片获取地址
+// export let pyImgUrl = 'http://localhost:4001/v1/sys/pyimages/';  // 拼音图片获取地址
 
-export let pyAudioUrl = 'https://xyw.htapi.pub/sys/pyaudios/';  // 拼音语音获取地址
-// export let pyAudioUrl = 'http://localhost:8001/sys/pyaudios/';  // 拼音语音获取地址
+export let pyAudioUrl = 'https://xyw.htapi.pub/v1/sys/pyaudios/';  // 拼音语音获取地址
+// export let pyAudioUrl = 'http://localhost:4001/v1/sys/pyaudios/';  // 拼音语音获取地址
 
-export let cyAudioUrl = 'https://xyw.htapi.pub/sys/cyaudios/';  // 成语语音获取地址
-// export let cyAudioUrl = 'http://localhost:8001/sys/cyaudios/';  // 成语语音获取地址
+export let cyAudioUrl = 'https://xyw.htapi.pub/v1/sys/cyaudios/';  // 成语语音获取地址
+// export let cyAudioUrl = 'http://localhost:4001/v1/sys/cyaudios/';  // 成语语音获取地址
 
-export let tyAudioUrl = 'https://xyw.htapi.pub/sys/tyaudios/';  // 听音选字语音获取地址
-// export let tyAudioUrl = 'http://localhost:8001/sys/tyaudios/';  // 听音选字语音获取地址
+export let tyAudioUrl = 'https://xyw.htapi.pub/v1/sys/tyaudios/';  // 听音选字语音获取地址
+// export let tyAudioUrl = 'http://localhost:4001/v1/sys/tyaudios/';  // 听音选字语音获取地址
 
-export let scAudioUrl = 'https://xyw.htapi.pub/sys/scaudios/';  // 诗词语音获取地址
-// export let scAudioUrl = 'http://localhost:8001/sys/scaudios/';  // 诗词语音获取地址
+export let scAudioUrl = 'https://xyw.htapi.pub/v1/sys/scaudios/';  // 诗词语音获取地址
+// export let scAudioUrl = 'http://localhost:4001/v1/sys/scaudios/';  // 诗词语音获取地址
